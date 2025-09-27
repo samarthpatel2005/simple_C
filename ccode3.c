a22feaed3ab00937d558ad833ccd58822c0577d2
SAMARTH <160775832+samarthpatel2005@users.noreply.github.com>
2025-09-27 16:45:36 +0530

    Implement Fibonacci sequence in ccode3.c

---
+++ ccode3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n = 7, a = 0, b = 1;
+    printf("Fibonacci: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        int next = a + b;
+        a = b; b = next;
+    }
+    return 0;
+}
